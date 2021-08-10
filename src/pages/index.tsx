15d5324de2099cc7df291b17dce42694b66c438b
Mateus Morais <mateus.batista010@gmail.com>
2021-08-10 20:49:40 -0300

    fixes on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import logo from '../../public/images/logo.svg';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +29,70 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Spacetraveling | Home</title>
+      </Head>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <Image src={logo} alt="logo" height={26} />
+        </div>
+      </header>
+      <main className={styles.mainContainer}>
+        <div className={styles.posts}>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <footer>
+              <span className={styles.postInfo}>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span className={styles.postInfo}>
+                <FiUser />
+                <span>Mateus Morais</span>
+              </span>
+            </footer>
+          </a>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <footer>
+              <span className={styles.postInfo}>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span className={styles.postInfo}>
+                <FiUser />
+                <span>Mateus Morais</span>
+              </span>
+            </footer>
+          </a>
+          <a>
+            <h1>Como utilizar hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <footer>
+              <span className={styles.postInfo}>
+                <FiCalendar />
+                <time>15 Mar 2021</time>
+              </span>
+              <span className={styles.postInfo}>
+                <FiUser />
+                <span>Mateus Morais</span>
+              </span>
+            </footer>
+          </a>
+        </div>
+        <footer>
+          <button>Carregar mais posts</button>
+        </footer>
+
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
